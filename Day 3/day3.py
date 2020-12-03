dbc0004aaaef58920c10fc7394865ab4b2485ac8
Bartosz Maziarka <barmazprzgit@gmail.com>
2020-12-04 00:57:40 +0100

    Added comments to day3

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -2,16 +2,23 @@ from functools import reduce
 
 
 def count_trees(input_file, slopes):
+    # we need line length to wrap around the input
     line_length = len(input_file.readline().strip('\n'))
+    # array of current line indexes for each slope
     indexes = [0]*len(slopes)
+    # array of numbers of trees for each slope
     values = [0]*len(slopes)
+    # we iterate over the file without the 1st line, because we've already read it while calculating line_length
     for line_idx, line in enumerate(input_file.read().splitlines()):
+        # we iterate over the slopes r - step right, d - step down
         for idx, (r, d) in enumerate(slopes):
+            # we subtract one because we've started for the second line, if the step > 1, we need to check it only every <d>nd/rd/th line
             if (line_idx-1) % d == 0:
+                # new index increased by step right value
                 new_index = (indexes[idx] + r) % line_length
                 indexes[idx] = new_index
+                # if the character under the current index is a tree, we increment tree count for the current slope
                 values[idx] += line[new_index] == '#'
-    print([indexes, values])
     return(reduce(lambda x, y: x*y, values))
 
 
