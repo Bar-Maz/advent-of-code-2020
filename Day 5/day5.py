44a98ea03f4798653ce705b7154da2fcc9f4cca4
Bartosz Maziarka <barmazprzgit@gmail.com>
2020-12-06 01:58:55 +0100

    Added day 5 part 1 and part 2 solutions

---
+++ Day 5/day5.py
@@ -0,0 +1,38 @@
+import re
+from itertools import tee
+
+def convert_to_binary(seat):
+    # replace B and R (upper halves) with 1 and F and L (lower halves) with 0, then convert it to a base 2 integer
+    return int(re.sub(r"[BR]", "1", re.sub(r"[FL]", "0", seat)), 2)
+
+
+def get_pairs(list):
+    # tee returns as many iterators based on a iterable as needed
+    a, b = tee(list)
+    # skip the first element of the second iterator
+    next(b)
+    # zip returns an array of tuples based on given iterables
+    # this array looks like this: [(list[0], list[1]), (list[1], list[2]) ...]
+    return zip(a, b)   
+
+# get max seat number from all the given seats encoded with F, B, L, R
+def part1(lines):
+    max = 0
+    for seat in lines:
+        seat_nr = convert_to_binary(seat)
+        if seat_nr > max:
+            max = seat_nr
+    return max
+
+# sort all seats, get pairs of them and search for a free seat
+def part2(lines): 
+    for pair in get_pairs(sorted([convert_to_binary(seat) for seat in lines])):
+        if pair[0] + 1 != pair[1]:
+            return pair[0] + 1
+    return None
+
+
+input_file = open("input.txt", 'r')
+lines = input_file.readlines()
+print("PART 1: ", part1(lines))
+print("PART 2: ", part2(lines))
--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Repository for my solutions to Advent of Code 2020 tasks. I realize that that so
 | [Day 2](https://adventofcode.com/2020/day/2)   | ⭐⭐ |
 | [Day 3](https://adventofcode.com/2020/day/3)   | ⭐⭐ |
 | [Day 4](https://adventofcode.com/2020/day/4)   | ⭐⭐ |
+| [Day 5](https://adventofcode.com/2020/day/5)   | ⭐⭐ |
