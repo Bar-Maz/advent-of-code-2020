cd3baf812fa4092f54e2223539709d97ee054b12
Bartosz Maziarka <barmazprzgit@gmail.com>
2020-12-13 02:57:24 +0100

    Added day 10 part 1 and part 2 solutions

---
+++ Day 10/day10.py
@@ -0,0 +1,62 @@
+from functools import reduce
+
+
+def get_sequences_count(numbers):
+    # example:
+    # adapters: 1,2,4,6:
+    # you can jump by 3 or less jolts
+    # you can get to 6 jolts adapter only from 4 jolts adapter, so x(6) = x(4)
+    # you can get to 4 jolts adapter from 1 and 2 jolts adapters, so x(4) = x(2) + x(1)
+    # you can get to 2 jolts adapter from the socket and from 1 jolts adapter, so x(2) = x(1) + x(0)
+    # YES, YOU CAN JUMP FROM 0 TO 2, YOU DON'T HAVE TO START FROM 1, THIS TOOK ME LIKE 2 DAYS OF
+    # GETTING THE SAME  WRONG RESULTS IN DIFFERENT WAYS TO REALIZE
+    # you can get to 1 jolts adapter only from the socket, so x(1) = x(0) = 1
+    # x(2) = x(1) + x(0) = 2
+    # x(4) = x(2) + x(1) = 3
+    # x(6) = x(4) = 3
+    # sequences: 0 1 2 4 6 | 0 2 4 6 | 0 1 2 6
+
+    # so anyway, you start from the socket - joltage = 0
+    sequences = {0: 1}
+    # get the max joltage
+    max_joltage = numbers[-1]
+    # iterate over the adapters
+    for num in numbers:
+        # initialize each entry, so we can add to it later
+        sequences[num] = 0
+        # check for joltages 3 or less lower than current
+        for x in range(1, 4):
+            joltage = num - x
+            # add to the current adapter (we don't know if given adapter exists, so we use get function to get the value)
+            sequences[num] += sequences.get(joltage, 0)
+    return sequences[max_joltage]
+
+
+def part_1(numbers):
+    # just count the differences between consecutive adapters
+    diff_1 = 0
+    # there's one more 3 jolts difference between the last adapter and your device, we count that one too
+    diff_3 = 1
+    jolts = 0
+    for num in numbers:
+        # check difference and increment the correct variable
+        diff = num - jolts
+        if diff == 1:
+            diff_1 += 1
+        elif diff == 3:
+            diff_3 += 1
+        # save current jolts for the next adapter
+        jolts = num
+    return diff_1 * diff_3
+
+
+def part_2(lines):
+    sequences_count = get_sequences_count(numbers)
+    return sequences_count
+
+
+input_file = open("input.txt", 'r')
+lines = input_file.read().splitlines()
+numbers = sorted([int(x) for x in lines])
+print("PART 1: ", part_1(numbers))
+print("PART 2: ", part_2(numbers))
--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Repository for my solutions to Advent of Code 2020 tasks. I realize that that so
 # Progress:
 | Day                                            | Stars | Day                                            | Stars |
 | -----------------------------------------------|:-----:| -----------------------------------------------|:-----:|
-| [Day 1](https://adventofcode.com/2020/day/1)   | ⭐⭐ |[Day 9](https://adventofcode.com/2020/day/9)    |  ⭐⭐ |
-| [Day 2](https://adventofcode.com/2020/day/2)   | ⭐⭐ |
-| [Day 3](https://adventofcode.com/2020/day/3)   | ⭐⭐ |
-| [Day 4](https://adventofcode.com/2020/day/4)   | ⭐⭐ |
-| [Day 5](https://adventofcode.com/2020/day/5)   | ⭐⭐ |
-| [Day 6](https://adventofcode.com/2020/day/6)   | ⭐⭐ |
-| [Day 7](https://adventofcode.com/2020/day/7)   | ⭐⭐ |
-| [Day 8](https://adventofcode.com/2020/day/8)   | ⭐⭐ |
+| [Day 01](https://adventofcode.com/2020/day/1)   | ⭐⭐ |[Day 09](https://adventofcode.com/2020/day/9)    |  ⭐⭐ |
+| [Day 02](https://adventofcode.com/2020/day/2)   | ⭐⭐ |[Day 10](https://adventofcode.com/2020/day/10)    |  ⭐⭐ |
+| [Day 03](https://adventofcode.com/2020/day/3)   | ⭐⭐ |
+| [Day 04](https://adventofcode.com/2020/day/4)   | ⭐⭐ |
+| [Day 05](https://adventofcode.com/2020/day/5)   | ⭐⭐ |
+| [Day 06](https://adventofcode.com/2020/day/6)   | ⭐⭐ |
+| [Day 07](https://adventofcode.com/2020/day/7)   | ⭐⭐ |
+| [Day 08](https://adventofcode.com/2020/day/8)   | ⭐⭐ |
